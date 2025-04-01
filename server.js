e5ece872fcbf4cf296a7a41b040ac7d7d0bfdbbe
nada <nadamansoursamir@gmail.com>
2025-04-01 23:10:31 +0200

    initial commit

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app.post('/get-token', (req, res) => {
   res.json({ token });
 });
 
-app.listen(3000, () => {
-  console.log('✅ Token server running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`✅ Token server running on port ${port}`);
 });
+
